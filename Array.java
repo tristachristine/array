04aba03cee1323b08932dd356f146d57701675c0
Trista <trista.yaskus@malad.us>
2025-09-18 18:53:50 -0600

    second commit

--- Array.java
+++ Array.java
@@ -6,12 +6,12 @@ public class Array {
         int[] Array = new int[100]; // Define array with size 100.
         {
             for (int i = 0; i < Array.length; i++) {
-                Array[i] = (i + 1);
+                Array[i] = (i + 1); // Fill array.
             }
         }
-        for (int x = 0; x < Array.length; x++) {
+        for (int x = 0; x < Array.length; x++) { // Repeats array contents.
 
-            if (Array[x] % 2 == 0) {
+            if (Array[x] % 2 == 0) { // Determines whether even or odd.
                 System.out.println(Array[x] + " is an even number.");
             } else {
                 System.out.println(Array[x] + " is an odd number.");
